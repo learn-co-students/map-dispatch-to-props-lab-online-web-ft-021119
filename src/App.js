54f86afabc0ce2feb7758132ada21e8466319073
github username <github email address>
2019-07-10 20:36:15 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
+import { addRestaurant } from  './actions/restaurants';
 
 export class App extends Component {
   render() {
@@ -13,4 +14,18 @@ export class App extends Component {
   }
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    restaurants: state.restaurants
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: () => {
+      dispatch(addRestaurant())
+    }
+  };
+};
+
+export default App
